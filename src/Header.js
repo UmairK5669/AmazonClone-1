c31fcfe1bbe012e9e7b0373c9a9f2e005104a5a4
Umair Khan <umairkhan5669@gmail.com>
2023-09-17 21:31:47 -0400

    First commit

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,7 @@ import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "./StateProvider";
 import { auth } from "./firebase";
-import logo from "./images/logoA.jpg";
+import logo from "./images/logo.jpg";
 function Header() {
   const [{ basket, user }, dispatch] = useStateValue();
 
